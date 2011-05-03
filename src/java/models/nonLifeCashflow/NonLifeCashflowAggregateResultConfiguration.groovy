fcc2d2675e86971133393e02da946f3dd4660abf
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2011-05-03 08:36:44 +0200

    PMO-1565: allow multiple indices selection and enable output of aggregate indices at start of period

--- src/java/models/nonLifeCashflow/NonLifeCashflowAggregateResultConfiguration.groovy
+++ src/java/models/nonLifeCashflow/NonLifeCashflowAggregateResultConfiguration.groovy
@@ -5,7 +5,16 @@ displayName = "Aggregate Gross Claims"
 components {
 	claimsGenerators {
 		subsubcomponents {
+			outClaimNumber = "AGGREGATED"
 			outClaims = "AGGREGATED"
+			outSeverityIndexApplied = "AGGREGATED"
+		}
+	}
+	underwritingSegments {
+		subsubcomponents {
+			outPolicyIndexApplied = "AGGREGATED"
+			outPremiumIndexApplied = "AGGREGATED"
+			outUnderwritingInfo = "AGGREGATED"
 		}
 	}
 }
