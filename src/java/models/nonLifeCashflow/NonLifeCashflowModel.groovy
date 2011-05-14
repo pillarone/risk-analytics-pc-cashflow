43e20cc1f290fa0484c54c211fad3167723efcc7
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2011-05-14 14:10:26 +0200

    PMO-1578: evaluate all patterns to define the number of periods

--- src/java/models/nonLifeCashflow/NonLifeCashflowModel.groovy
+++ src/java/models/nonLifeCashflow/NonLifeCashflowModel.groovy
@@ -14,11 +14,16 @@ import org.pillarone.riskanalytics.domain.pc.cf.pattern.Pattern
 import org.pillarone.riskanalytics.domain.pc.cf.pattern.Patterns
 import org.pillarone.riskanalytics.domain.pc.cf.dependency.Dependencies
 import org.pillarone.riskanalytics.domain.pc.cf.dependency.MultipleDependencies
+import org.pillarone.riskanalytics.domain.pc.cf.exposure.RiskBands
+import org.apache.commons.logging.LogFactory
+import org.apache.commons.logging.Log
 
 /**
  * @author stefan.kunz (at) intuitive-collaboration (dot) com
  */
 class NonLifeCashflowModel extends StochasticModel {
+
+    private static Log LOG = LogFactory.getLog(NonLifeCashflowModel);
 
     GlobalParameters globalParameters
     Indices indices
@@ -65,24 +70,42 @@ class NonLifeCashflowModel extends StochasticModel {
 
     private Period lastPatternPeriod() {
         Period maxPeriods = Period.months(0);
-        Map<String, Period> patternLengths = new HashMap<String, Period>()
-        for (Pattern pattern: patterns.subPayoutPatterns.componentList) {
+
+        Map<String, Period> claimsGeneratorPatternLengths = new HashMap<String, Period>()
+        for (Pattern pattern: patterns.subPayoutPatterns.componentList + patterns.subReportingPatterns.componentList) {
             Period period = pattern.parmPattern.pattern.getLastCumulativePeriod()
-            patternLengths.put(pattern.name, period)
+            LOG.debug("payout/reporting pattern $pattern.name $period.months")
+            claimsGeneratorPatternLengths.put(pattern.name, period)
         }
 
-        if (!patternLengths.isEmpty()) {
+        if (!claimsGeneratorPatternLengths.isEmpty()) {
             for (ClaimsGenerator generator: claimsGenerators.componentList) {
-                Period period = patternLengths.get(generator.parmPayoutPattern?.stringValue)
+                Period period = claimsGeneratorPatternLengths.get(generator.parmPayoutPattern?.stringValue)
                 if (period != null) {
                     maxPeriods = Period.months(Math.max(maxPeriods.months, period.months))
                 }
-                period = patternLengths.get(generator.parmReportingPattern?.stringValue)
+                period = claimsGeneratorPatternLengths.get(generator.parmReportingPattern?.stringValue)
                 if (period != null) {
                     maxPeriods = Period.months(Math.max(maxPeriods.months, period.months))
                 }
             }
         }
+
+        Map<String, Period> premiumPatternLengths = new HashMap<String, Period>()
+        for (Pattern pattern: patterns.subPremiumPatterns.componentList) {
+            Period period = pattern.parmPattern.pattern.getLastCumulativePeriod()
+            LOG.debug("premium pattern $pattern.name $period.months")
+            premiumPatternLengths.put(pattern.name, period)
+        }
+        if (!premiumPatternLengths.isEmpty()) {
+            for (RiskBands riskBands: underwritingSegments.componentList) {
+                Period period = premiumPatternLengths.get(riskBands.parmPremiumPattern?.stringValue)
+                if (period != null) {
+                    maxPeriods = Period.months(Math.max(maxPeriods.months, period.months))
+                }
+            }
+        }
+        LOG.debug("max periods: $maxPeriods")
         return maxPeriods
     }
 
