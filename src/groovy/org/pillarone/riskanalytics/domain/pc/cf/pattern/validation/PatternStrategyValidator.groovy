4c1bd971ca14ce74d20449a97a27257845457784
Jessika Walter <jessika.walter@intuitive-collaboration.com>
2011-06-07 14:54:43 +0200

    PMO-1577: cross-validation of reporting and payout pattern (related within ClaimsGenerator); payout pattern must be smaller than reporting pattern

--- src/groovy/org/pillarone/riskanalytics/domain/pc/cf/pattern/validation/PatternStrategyValidator.groovy
+++ src/groovy/org/pillarone/riskanalytics/domain/pc/cf/pattern/validation/PatternStrategyValidator.groovy
@@ -12,6 +12,10 @@ import org.pillarone.riskanalytics.core.simulation.item.parameter.ParameterHolde
 import org.pillarone.riskanalytics.core.simulation.item.parameter.ParameterObjectParameterHolder
 import org.pillarone.riskanalytics.core.parameterization.IParameterObjectClassifier
 import org.pillarone.riskanalytics.domain.pc.cf.pattern.PatternTableConstraints
+import org.pillarone.riskanalytics.domain.pc.cf.pattern.PatternPacket
+import org.pillarone.riskanalytics.core.simulation.item.parameter.ConstrainedStringParameterHolder
+import org.joda.time.Period
+import org.pillarone.riskanalytics.domain.utils.validation.ParameterValidationImpl
 
 /**
  * @author jessika.walter (at) intuitive-collaboration (dot) com
@@ -32,6 +36,16 @@ class PatternStrategyValidator implements IParameterizationValidator {
 
         List<ParameterValidation> errors = []
 
+        /** key: path             */
+        Map<String, PatternPacket> payoutPatterns = [:]
+        /** key: path             */
+        Map<String, PatternPacket> reportingPatterns = [:]
+        /** key: path             */
+        Map<String, String> reportingPatternPerClaimsGenerator = [:]
+        /** key: path             */
+        Map<String, String> payoutPatternPerClaimsGenerator = [:]
+
+
         for (ParameterHolder parameter in parameters) {
             if (parameter instanceof ParameterObjectParameterHolder) {
                 IParameterObjectClassifier classifier = parameter.getClassifier()
@@ -39,13 +53,46 @@ class PatternStrategyValidator implements IParameterizationValidator {
                     if (LOG.isDebugEnabled()) {
                         LOG.debug "validating ${parameter.path}"
                     }
+                    if (parameter.path.contains("patterns:subPayoutPatterns:")) {
+                        payoutPatterns[parameter.path - 'patterns:subPayoutPatterns:' - ':parmPattern'] = parameter.getBusinessObject().getPattern()
+                    }
+                    if (parameter.path.contains("patterns:subReportingPatterns:")) {
+                        reportingPatterns[parameter.path - 'patterns:subReportingPatterns:' - ':parmPattern'] = parameter.getBusinessObject().getPattern()
+                    }
+
                     def currentErrors = validationService.validate(classifier, parameter.getParameterMap())
                     currentErrors*.path = parameter.path
                     errors.addAll(currentErrors)
                 }
                 errors.addAll(validate(parameter.classifierParameters.values().toList()))
             }
+
+            else if (parameter instanceof ConstrainedStringParameterHolder && parameter.path.contains("claimsGenerators")) {
+                if (parameter.path.contains("parmReportingPattern")) {
+                    reportingPatternPerClaimsGenerator[parameter.path - ':parmReportingPattern'] = parameter.value.getStringValue()
+                }
+                else if (parameter.path.contains("parmPayoutPattern")) {
+                    payoutPatternPerClaimsGenerator[parameter.path - ':parmPayoutPattern'] = parameter.value.getStringValue()
+                }
+
+            }
         }
+
+        for (String claimsGeneratorPath: reportingPatternPerClaimsGenerator.keySet()) {
+            PatternPacket reportingPattern = reportingPatterns[reportingPatternPerClaimsGenerator[claimsGeneratorPath]]
+            PatternPacket payoutPattern = payoutPatterns[payoutPatternPerClaimsGenerator[claimsGeneratorPath]]
+            TreeMap<Integer, Double> reportingValuesPerMonth = getCumulativeValuePerMonth(reportingPattern)
+            TreeMap<Integer, Double> payoutValuesPerMonth = getCumulativeValuePerMonth(payoutPattern)
+            for (Map.Entry<Integer, Double> payoutEntry: payoutValuesPerMonth.entrySet()) {
+                if (payoutEntry.value <= reportingValuesPerMonth.floorEntry(payoutEntry.key).value) continue
+                ParameterValidationImpl error = new ParameterValidationImpl(ValidationType.ERROR,
+                        'claims.generator.reporting.pattern.smaller.than.payout.pattern',
+                        [payoutEntry.key, payoutEntry.value, reportingValuesPerMonth.floorEntry(payoutEntry.key).value])
+                error.path = claimsGeneratorPath + ':parmPayoutPattern'
+                errors << error
+            }
+        }
+
         return errors
     }
 
@@ -136,5 +183,23 @@ class PatternStrategyValidator implements IParameterizationValidator {
             return true
         }
     }
+
+    private TreeMap<Integer, Double> getCumulativeValuePerMonth(PatternPacket pattern) {
+        TreeMap<Integer, Double> valuePerMonth = new TreeMap<Integer, Double>();
+        if (pattern == null) {
+            valuePerMonth.put(0, 1d)
+            return valuePerMonth
+        }
+        List<Period> periods = pattern.getCumulativePeriods()
+        List<Double> values = pattern.getCumulativeValues()
+        for (int i = 0; i < periods.size(); i++) {
+            valuePerMonth.put(periods[i].months, values[i])
+        }
+        if (valuePerMonth.firstKey() > 0) {
+            valuePerMonth.put(0, 0)
+        }
+        return valuePerMonth
+    }
+
 }
 
