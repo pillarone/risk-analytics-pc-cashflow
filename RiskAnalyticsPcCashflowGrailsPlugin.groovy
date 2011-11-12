6a76d54443786d8e3073ff56b0fd929e07fd9ac4
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2011-11-12 14:44:49 +0100

    releasing new plugin version 0.1.23

--- RiskAnalyticsPcCashflowGrailsPlugin.groovy
+++ RiskAnalyticsPcCashflowGrailsPlugin.groovy
@@ -34,7 +34,7 @@ import org.pillarone.riskanalytics.domain.pc.cf.exposure.UnderwritingInfoPacketA
 
 class RiskAnalyticsPcCashflowGrailsPlugin {
     // the plugin version
-    def version = "0.1.22"
+    def version = "0.1.23"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
