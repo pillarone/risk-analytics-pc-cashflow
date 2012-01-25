3d8769a012ad33595a6a3bed07ae33a6a739c1c5
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2012-01-25 12:13:07 +0100

    PMO-1944: adding project specific collectors to CollectorMapping

---
+++ grails-app/conf/PCCashflowBootStrap.groovy
@@ -0,0 +1,30 @@
+import org.pillarone.riskanalytics.core.output.CollectorMapping
+import org.pillarone.riskanalytics.domain.pc.cf.output.AggregateSplitPerSourceCollectingModeStrategy
+import org.pillarone.riskanalytics.domain.pc.cf.output.AggregateUltimateClaimCollectingModeStrategy
+import org.pillarone.riskanalytics.domain.pc.cf.output.AggregateUltimateReportedPaidClaimCollectingModeStrategy
+import org.pillarone.riskanalytics.domain.pc.cf.output.AggregateUltimatePaidClaimCollectingModeStrategy
+
+class PCCashflowBootStrap {
+
+    def init = {servletContext ->
+
+        CollectorMapping.withTransaction { status ->
+            def c = new CollectorMapping(collectorName: AggregateSplitPerSourceCollectingModeStrategy.IDENTIFIER);
+            if (CollectorMapping.find(c) == null)
+                c.save()
+            c = new CollectorMapping(collectorName: AggregateUltimateClaimCollectingModeStrategy.IDENTIFIER);
+            if (CollectorMapping.find(c) == null)
+                c.save()
+            c = new CollectorMapping(collectorName: AggregateUltimateReportedPaidClaimCollectingModeStrategy.IDENTIFIER);
+            if (CollectorMapping.find(c) == null)
+                c.save()
+            c = new CollectorMapping(collectorName: AggregateUltimatePaidClaimCollectingModeStrategy.IDENTIFIER);
+            if (CollectorMapping.find(c) == null)
+                c.save()
+        }
+
+    }
+
+    def destroy = {
+    }
+}
