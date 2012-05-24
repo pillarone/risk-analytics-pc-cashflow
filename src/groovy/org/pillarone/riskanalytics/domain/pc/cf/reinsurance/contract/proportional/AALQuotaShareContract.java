dc52ec4ae400c4bc9bcb7266d52215c8f6163a8f
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2012-05-24 09:34:37 +0200

    cleaning up and splitting segment net figures by source in detail drill down view

--- src/groovy/org/pillarone/riskanalytics/domain/pc/cf/reinsurance/contract/proportional/AALQuotaShareContract.java
+++ src/groovy/org/pillarone/riskanalytics/domain/pc/cf/reinsurance/contract/proportional/AALQuotaShareContract.java
@@ -1,12 +1,6 @@
 package org.pillarone.riskanalytics.domain.pc.cf.reinsurance.contract.proportional;
 
-import org.pillarone.riskanalytics.core.simulation.IPeriodCounter;
-import org.pillarone.riskanalytics.domain.pc.cf.claim.BasedOnClaimProperty;
-import org.pillarone.riskanalytics.domain.pc.cf.claim.ClaimCashflowPacket;
-import org.pillarone.riskanalytics.domain.pc.cf.claim.ClaimUtils;
-import org.pillarone.riskanalytics.domain.pc.cf.reinsurance.contract.ClaimStorage;
 import org.pillarone.riskanalytics.domain.pc.cf.reinsurance.contract.limit.AalLimitStrategy;
-import org.pillarone.riskanalytics.domain.pc.cf.reinsurance.contract.nonproportional.ThresholdStore;
 import org.pillarone.riskanalytics.domain.pc.cf.reinsurance.contract.proportional.commission.ICommission;
 
 /**
@@ -14,51 +8,7 @@ import org.pillarone.riskanalytics.domain.pc.cf.reinsurance.contract.proportiona
  */
 public class AALQuotaShareContract extends AALAADQuotaShareContract {
 
-//    private ThresholdStore periodLimit;
-
     public AALQuotaShareContract(double quotaShare, ICommission commission, AalLimitStrategy limit) {
         super(quotaShare, commission, limit);
-//        periodLimit = new ThresholdStore(limit.getAAL());
     }
-
-//    public ClaimCashflowPacket calculateClaimCeded(ClaimCashflowPacket grossClaim, ClaimStorage storage, IPeriodCounter periodCounter) {
-//        double quotaShareUltimate = 0;
-//        if (!storage.hasReferenceCeded()) {
-//            quotaShareUltimate = adjustedQuote(grossClaim.developedUltimate(), grossClaim.nominalUltimate(),
-//                    BasedOnClaimProperty.ULTIMATE, storage);
-//            storage.lazyInitCededClaimRoot(quotaShareUltimate);
-//        }
-//
-//        double quotaShareReported = adjustedQuote(grossClaim.getReportedCumulatedIndexed(),
-//                grossClaim.getReportedIncrementalIndexed(), BasedOnClaimProperty.REPORTED, storage);
-//        double quotaSharePaid = adjustedQuote(grossClaim.getPaidCumulatedIndexed(),
-//                grossClaim.getPaidIncrementalIndexed(), BasedOnClaimProperty.PAID, storage);
-//        ClaimCashflowPacket cededClaim = ClaimUtils.getCededClaim(grossClaim, storage, quotaShareUltimate,
-//                quotaShareReported, quotaSharePaid, true);
-//        add(grossClaim, cededClaim);
-//        return cededClaim;
-//    }
-//
-//    protected double adjustedQuote(double claimPropertyCumulated, double claimPropertyIncremental,
-//                                   BasedOnClaimProperty claimPropertyBase, ClaimStorage storage) {
-//        double limit = periodLimit.get(claimPropertyBase);
-//        double ceded = -claimPropertyCumulated * quotaShare;
-//        double incrementalCeded = ceded - storage.getCumulatedCeded(claimPropertyBase);
-//        double cededAfterAAL = limit > incrementalCeded ? incrementalCeded : limit;
-//        periodLimit.plus(-cededAfterAAL, claimPropertyBase);
-//        return claimPropertyIncremental == 0 ? 0 : cededAfterAAL / claimPropertyIncremental;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        StringBuffer buffer = new StringBuffer();
-//        buffer.append(super.toString());
-//        buffer.append(", AAL ultimate: ");
-//        buffer.append(periodLimit.get(BasedOnClaimProperty.ULTIMATE));
-//        buffer.append(", AAL reported: ");
-//        buffer.append(periodLimit.get(BasedOnClaimProperty.REPORTED));
-//        buffer.append(", AAL paid: ");
-//        buffer.append(periodLimit.get(BasedOnClaimProperty.PAID));
-//        return buffer.toString();
-//    }
 }
