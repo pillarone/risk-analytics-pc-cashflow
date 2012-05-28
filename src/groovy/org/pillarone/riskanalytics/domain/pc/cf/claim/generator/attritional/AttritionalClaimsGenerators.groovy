1011aa0236f7d08e4d0fdf938bad9653c300ac52
Simon Parten <simon.parten@art-allianz.com>
2012-05-28 16:04:19 +0200

    Bump version

--- src/groovy/org/pillarone/riskanalytics/domain/pc/cf/claim/generator/attritional/AttritionalClaimsGenerators.groovy
+++ src/groovy/org/pillarone/riskanalytics/domain/pc/cf/claim/generator/attritional/AttritionalClaimsGenerators.groovy
@@ -8,20 +8,44 @@ import org.pillarone.riskanalytics.domain.pc.cf.indexing.FactorsPacket
 import org.pillarone.riskanalytics.domain.pc.cf.pattern.PatternPacket
 import org.pillarone.riskanalytics.domain.pc.cf.claim.ClaimCashflowPacket
 import org.pillarone.riskanalytics.core.components.MultiPhaseDynamicComposedComponent
+import org.pillarone.riskanalytics.domain.pc.cf.claim.generator.AbstractClaimsGenerator
+import org.pillarone.riskanalytics.domain.pc.cf.accounting.experienceAccounting.CommutationState
+import org.pillarone.riskanalytics.core.components.MultiPhaseComponent
+import org.pillarone.riskanalytics.core.components.MultiPhaseComposedComponent
+import org.pillarone.riskanalytics.core.components.DynamicMultiPhaseComposedComponent
 
 /**
  * @author stefan.kunz (at) intuitive-collaboration (dot) com
  */
-class AttritionalClaimsGenerators extends DynamicComposedComponent {
+class AttritionalClaimsGenerators extends DynamicMultiPhaseComposedComponent {
 
     PacketList<UnderwritingInfoPacket> inUnderwritingInfo = new PacketList<UnderwritingInfoPacket>(UnderwritingInfoPacket)
     PacketList<FactorsPacket> inFactors = new PacketList<FactorsPacket>(FactorsPacket)
     PacketList<PatternPacket> inPatterns = new PacketList<PatternPacket>(PatternPacket)
     PacketList<ClaimCashflowPacket> outClaims = new PacketList<ClaimCashflowPacket>(ClaimCashflowPacket.class)
 
+    protected PacketList<CommutationState> inCommutationState = new PacketList<CommutationState>(CommutationState.class)
+
     @Override
     Component createDefaultSubComponent() {
         new AttritionalClaimsGenerator()
+    }
+
+    protected void doCalculation(String phase) {
+        for (Component component : componentList) {
+            ((MultiPhaseComposedComponent) component).doCalculation phase
+        }
+    }
+
+    void allocateChannelsToPhases() {
+        setTransmitterPhaseInput(inUnderwritingInfo, AbstractClaimsGenerator.PHASE_CLAIMS_CALCULATION )
+        setTransmitterPhaseInput(inFactors, AbstractClaimsGenerator.PHASE_CLAIMS_CALCULATION )
+        setTransmitterPhaseInput(inPatterns, AbstractClaimsGenerator.PHASE_CLAIMS_CALCULATION )
+
+        setTransmitterPhaseOutput(outClaims, AbstractClaimsGenerator.PHASE_CLAIMS_CALCULATION )
+
+        setTransmitterPhaseInput(inCommutationState, AbstractClaimsGenerator.PHASE_STORE_COMMUTATION_STATE )
+
     }
 
     @Override
