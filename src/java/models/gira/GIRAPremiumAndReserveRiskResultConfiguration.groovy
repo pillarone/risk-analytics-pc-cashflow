a913b45f5a45eb12767a196ef3181658546c0aa9
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2012-06-20 20:56:53 +0200

    PMO-1992: adding slimmer result configurations

---
+++ src/java/models/gira/GIRAPremiumAndReserveRiskResultConfiguration.groovy
@@ -0,0 +1,14 @@
+package models.gira
+
+model = models.gira.GIRAModel
+displayName = "Premium And Reserve Risk (Segment)"
+components {
+	segments {
+		outClaimsGross = "PREMIUM_RESERVE_RISK_TRIANGLE"
+		outClaimsNet = "PREMIUM_RESERVE_RISK_TRIANGLE"
+		subsubcomponents {
+			outClaimsGross = "PREMIUM_RESERVE_RISK_TRIANGLE"
+			outClaimsNet = "PREMIUM_RESERVE_RISK_TRIANGLE"
+		}
+	}
+}
