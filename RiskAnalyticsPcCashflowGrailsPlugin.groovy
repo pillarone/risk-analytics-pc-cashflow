b5e4e96f14f9ebd6c7f2dc91489cfb987076bab7
Simon Parten <simon.parten@art-allianz.com>
2012-06-27 11:41:47 +0200

    Change consistency check to accept a range

--- RiskAnalyticsPcCashflowGrailsPlugin.groovy
+++ RiskAnalyticsPcCashflowGrailsPlugin.groovy
@@ -45,7 +45,7 @@ import org.pillarone.riskanalytics.domain.pc.cf.output.AggregateUltimateReported
 
 class RiskAnalyticsPcCashflowGrailsPlugin {
     // the plugin version
-    def version = "0.4.8.13"
+    def version = "0.4.8.14"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
