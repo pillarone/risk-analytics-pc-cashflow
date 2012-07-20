73abb38c02e96c7a97f2467957d5b7a0191c1a1d
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2012-07-20 19:27:12 +0200

    releasing new plugin version 0.4.8.19

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -25,8 +25,8 @@ grails.project.dependency.resolution = {
         test ":excel-import:0.9.6"
 
         if (appName == "risk-analytics-pc-cashflow") {
-            runtime "org.pillarone:risk-analytics-core:1.6-ALPHA-4.3"
-            runtime("org.pillarone:risk-analytics-commons:0.4.6") { transitive = false }
+            runtime "org.pillarone:risk-analytics-core:1.6-ALPHA-4.4"
+            runtime("org.pillarone:risk-analytics-commons:0.4.7") { transitive = false }
         }
     }
 }
