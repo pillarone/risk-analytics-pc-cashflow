bd6c72422d6ac6de13b1bd9ed035387de3a457e4
Simon Parten <simon.parten@art-allianz.com>
2012-10-22 16:16:34 +0200

    Bump plugin number in preparation for new release

--- RiskAnalyticsPcCashflowGrailsPlugin.groovy
+++ RiskAnalyticsPcCashflowGrailsPlugin.groovy
@@ -46,7 +46,7 @@ import org.pillarone.riskanalytics.domain.pc.cf.indexing.RunOffIndexSelectionTab
 
 class RiskAnalyticsPcCashflowGrailsPlugin {
     // the plugin version
-    def version = "0.4.21"
+    def version = "0.4.22"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
