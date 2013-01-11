6892bb7d462488c01a5b7052f325f74f3fc33a8a
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2013-01-11 10:54:42 +0100

    releasing new plugin version 0.5.7-kti

--- RiskAnalyticsPcCashflowGrailsPlugin.groovy
+++ RiskAnalyticsPcCashflowGrailsPlugin.groovy
@@ -50,7 +50,7 @@ import org.pillarone.riskanalytics.domain.pc.cf.output.SingleUltimatePaidClaimCo
 
 class RiskAnalyticsPcCashflowGrailsPlugin {
     // the plugin version
-    def version = "0.5.6-kti"
+    def version = "0.5.7-kti"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
