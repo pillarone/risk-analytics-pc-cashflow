6a8d7c3df6103f627bb284df1602fb62166b0647
Simon Parten <simon.parten@art-allianz.com>
2013-01-18 14:43:19 +0100

    ART-1043 - Finish implementation of period dependant copulae for the business logic in the acutal model. Final requirement is to implement the muli period copula strategy.

--- src/groovy/org/pillarone/riskanalytics/domain/pc/cf/claim/dependancy/DynamicDependancy.groovy
+++ src/groovy/org/pillarone/riskanalytics/domain/pc/cf/claim/dependancy/DynamicDependancy.groovy
@@ -1,6 +1,7 @@
 package org.pillarone.riskanalytics.domain.pc.cf.claim.dependancy
 
 import org.pillarone.riskanalytics.core.components.DynamicComposedComponent
+import org.pillarone.riskanalytics.domain.pc.cf.claim.generator.dependancy.MultiPeriodCopula
 
 /**
  * @author stefan.kunz (at) intuitive-collaboration (dot) com
