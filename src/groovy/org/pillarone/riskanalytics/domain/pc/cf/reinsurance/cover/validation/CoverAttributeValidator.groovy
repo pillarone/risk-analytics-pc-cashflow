2ba37e2da566b1d06a904d7df03caa24731ffe5a
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2013-02-11 14:28:58 +0100

    PMO-2369: make check more generic

--- src/groovy/org/pillarone/riskanalytics/domain/pc/cf/reinsurance/cover/validation/CoverAttributeValidator.groovy
+++ src/groovy/org/pillarone/riskanalytics/domain/pc/cf/reinsurance/cover/validation/CoverAttributeValidator.groovy
@@ -102,7 +102,7 @@ class CoverAttributeValidator extends AbstractParameterizationValidator {
     }
 
     private List checkContracts(ConstrainedMultiDimensionalParameter parameter, String currentContractPath) {
-        if (parameter.getValues().size() < 6) return
+        if (parameter.getValues().size() < parameter.getColumnCount()) return
         List<String> netContracts = parameter.getColumn(CoverMap.CONTRACT_NET_OF_COLUMN_INDEX)
         List<String> cededContracts = parameter.getColumn(CoverMap.CONTRACT_CEDED_OF_COLUMN_INDEX)
         String conflictingContractName = null
