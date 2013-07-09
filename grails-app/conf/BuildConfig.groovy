0b34104ac9633e948057573621292a7ea3ee5d3b
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2013-07-09 13:00:13 +0200

    releasing plugin version 1.7

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -24,8 +24,8 @@ grails.project.dependency.resolution = {
         compile ":excel-import:1.0.0"
 
         if (appName == "risk-analytics-pc-cashflow") {
-            runtime "org.pillarone:risk-analytics-core:1.7-b1"
-            runtime("org.pillarone:risk-analytics-commons:1.7-b1") { transitive = false }
+            runtime "org.pillarone:risk-analytics-core:1.7"
+            runtime("org.pillarone:risk-analytics-commons:1.7") { transitive = false }
         }
     }
 
