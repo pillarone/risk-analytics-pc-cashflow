ab7d0294a8a3a28f5e2ba99d2a517e857df3a074
Stefan Kunz <stefan.kunz@intuitive-collaboration.com>
2013-09-11 11:16:58 +0200

    release new plugin version 1.7.4

--- RiskAnalyticsPcCashflowGrailsPlugin.groovy
+++ RiskAnalyticsPcCashflowGrailsPlugin.groovy
@@ -38,7 +38,7 @@ import org.pillarone.riskanalytics.domain.utils.constraint.*
 
 class RiskAnalyticsPcCashflowGrailsPlugin {
     // the plugin version
-    def version = "1.7.3"
+    def version = "1.7.4"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.2.1 > *"
     // the other plugins this plugin depends on
