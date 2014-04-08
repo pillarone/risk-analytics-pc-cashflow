be89837448eb1aa1477187148d2e5ff62e3cc405
Michael Spahn <michael.spahn@intuitive-collaboration.com>
2014-04-08 15:48:08 +0200

    upgrade quartz plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -22,7 +22,7 @@ grails.project.dependency.resolution = {
         runtime ":background-thread:1.3"
         runtime ":hibernate:3.6.10.3"
         runtime ":release:3.0.1"
-        runtime ":quartz:0.4.2"
+        runtime ":quartz:1.0.1"
         runtime ":spring-security-core:2.0-RC2"
         runtime ":tomcat:7.0.42"
 
