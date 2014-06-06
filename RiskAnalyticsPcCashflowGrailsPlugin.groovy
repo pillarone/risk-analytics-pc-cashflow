491c87ab0145d3490bfc1f9807043dc737064fa2
fazl <fazl.rahman@art-allianz.com>
2014-06-06 17:10:56 +0200

    risk-analytics-pc-cashflow-1.9.x v1.9.2 (propagate bumping core to 1.9.1)

--- RiskAnalyticsPcCashflowGrailsPlugin.groovy
+++ RiskAnalyticsPcCashflowGrailsPlugin.groovy
@@ -47,7 +47,7 @@ import org.pillarone.riskanalytics.domain.utils.constraint.*
 
 class RiskAnalyticsPcCashflowGrailsPlugin {
     // the plugin version
-    def version = "1.9.1"
+    def version = "1.9.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3.2 > *"
     // the other plugins this plugin depends on
